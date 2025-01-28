91482ce037d1092e9285401c66ad6d90862fbad6
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-01-28 15:37:22 +0100

    Changes to be committed: 	deleted:    public/assets/GitHub.png 	modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -81,6 +81,7 @@
             box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
         }
         .container h2 {
+            margin-top: 0;
             color: #6d5dfc;
         }
         .card {
@@ -96,6 +97,8 @@
             transform: translateY(-10px); /* Permet de faire monter les cartes quand survolé */
         }
         .card h3 {
+            margin-top: 0;
+            margin-bottom: 0;
             color: #5e96ff; /* Titre des cartes en bleu */
         }
         .card .date {
@@ -142,6 +145,51 @@
             margin-top: 3rem; /* Distance du dernier élément de la page */
             border-top: 3px solid #6d5dfc; /* Bordure du dessus de l'élément */
         }
+
+        #icons svg {
+            width: 5em;
+            height: 5em;
+            fill: #cccccc; /* Default icon color */
+        }
+
+        #icons {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        #icons a {
+            text-decoration: none;
+            color: #ffffff; /* Ensure text is visible */
+            display: inline-flex; 
+            padding: 0; /* Supprime tout padding éventuel */
+            margin: 0; /* Supprime les marges autour */
+        }
+
+        #icons figcaption {
+            margin: 0.5rem 0 0;
+            padding: 0; /* Supprime les paddings superflus */
+            text-align: center;
+        }       
+
+        #icons a figure {
+            margin-left: 0;
+            margin-right: 0;
+            margin-top: 0;
+            margin-bottom: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: max-content;
+        }
+
+
+        #icons a:hover figure svg{
+            stroke: var(--tertiary);
+            fill: var(--tertiary);
+            transition: .2s;
+        }
+
+
 
     </style>
 </head>
@@ -201,8 +249,23 @@
 
     <div class="container" id="contact">
         <h2>Pour me contacter</h2>
-        <a href="https://github.com/TevaPhilippe05"><img src="assets/GitHub.png" width=10% alt="GitHub"> </a>
+        <span id="icons">
+            <a href="https://github.com/TevaPhilippe05">
+                <figure>
+                    <title>Python</title>
+                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title>
+                    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+                    <figcaption>
+                        GitHub
+                    </figcaption>
+                </figure>
+            </a>
+        </span>
     </div>
+        
+
+
+
     <footer>
         <p>&copy; 2025 Teva Philippe. Tous droits réservés.</p>
     </footer>
